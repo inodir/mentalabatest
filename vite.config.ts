00665626d9caf42a139fb822ab5067e35783ae52
Ulugbek Erkinov <ulugbek9015y@gmail.com>
2026-02-10 02:34:53 +0500

    changes on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,47 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: true, // 0.0.0.0 / :: hammasidan kirishga ruxsat
+    port: 8569,
+    allowedHosts: ["test.statistics.uz"],
     hmr: {
       overlay: false,
     },
   },
+
+  preview: {
+    host: true,
+    port: 8569,
+    allowedHosts: ["test.statistics.uz"],
+  },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
